b3b5213c8a03c57a701a5dddbda4543cd8c0ecad
Devanik Debnath <162272415+Devanik21@users.noreply.github.com>
2024-08-22 20:02:32 +0530

    ℹ️ About.py

--- pages/ℹ️ About.py
+++ pages/ℹ️ About.py
@@ -3,9 +3,9 @@ import streamlit as st
 def show_about_page():
     st.title("About This Project")
     st.write("""
-    ## Stellar Minesite
+    ## Galactic Mining Hub
 
-    **Stellar Minesite** is an advanced tool designed to assist in the exploration and evaluation of potential mining sites across different celestial bodies.
+    **Galactic Mining Hub** is an advanced tool designed to assist in the exploration and evaluation of potential mining sites across different celestial bodies.
 
     ### Project Objectives
     - To predict and recommend potential mining sites based on key characteristics.
@@ -20,7 +20,7 @@ def show_about_page():
     - **Insights**: Provide actionable recommendations based on the data analysis.
 
     ### Contact
-    If you have any questions or feedback, please reach out to the development team.
+    If you have any questions or feedback, please reach out to https://www.linkedin.com/in/devanik/
     """)
 
 show_about_page()
