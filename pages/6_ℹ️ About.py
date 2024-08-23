e413746543cc109c49b3cf96cbd93cc3c661641a
Devanik Debnath <162272415+Devanik21@users.noreply.github.com>
2024-08-23 13:13:01 +0530

    Update 6_ℹ️ About.py

--- pages/6_ℹ️ About.py
+++ pages/6_ℹ️ About.py
@@ -13,7 +13,7 @@ def show_about_page():
     - **Recommend Sites**: Offer personalized recommendations based on user-defined criteria and site characteristics.
     - **Analyze Deeply**: Dive into rich datasets with tools for clustering, outlier detection, and more.
     - **Visualize Clearly**: Gain clarity with dynamic visualizations that make complex data understandable.
-    - **Drive Insights**: Empower decision-making with data-driven insights that highlight the most promising opportunities.
+    - **Drive Insights**: Empower decision-making with data-driven insights highlighting the most promising opportunities.
 
     ### 🔍 **Key Features**
     - **AI-Powered Predictions**: Automatically assess mining potential using sophisticated models.
@@ -23,10 +23,10 @@ def show_about_page():
     - **Strategic Insights**: Receive actionable advice on where to focus your mining efforts.
 
     ### 🌐 **Get in Touch**
-    Have questions, suggestions, or just want to connect? Reach out to me on [LinkedIn](https://www.linkedin.com/in/devanik/) and let’s chat about the future of space exploration!
+    Have questions, suggestions, or want to connect? Reach out to me on [LinkedIn](https://www.linkedin.com/in/devanik/) and let’s chat about the future of space exploration!
 
     ---
-    Built by passionate developers and space enthusiasts, **Galactic Mining Hub** is more than just a tool—it's your gateway to the stars.
+    Built by Devanik, a passionate developer and space enthusiast, **Galactic Mining Hub** is more than just a tool—it's your gateway to the stars.
     """)
 
 show_about_page()
