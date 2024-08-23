0cfad9278adccb108ea13bfca55c952982a66a58
Devanik Debnath <162272415+Devanik21@users.noreply.github.com>
2024-08-23 12:47:59 +0530

    1_🚀Predict.py

--- pages/1_🚀Predict.py
+++ pages/1_🚀Predict.py
@@ -35,7 +35,7 @@ st.markdown("""
             font-weight: bold;
         }
         .stButton>button:hover {
-            background-color: #65ba8b;
+            background-color: #10914a;
         }
     </style>
 """, unsafe_allow_html=True)
