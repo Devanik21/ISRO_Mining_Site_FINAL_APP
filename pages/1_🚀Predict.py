b83ca26caf6efae26658c541e799ce28d42e1c74
Devanik Debnath <162272415+Devanik21@users.noreply.github.com>
2025-02-05 13:27:17 +0530

    Update 1_🚀Predict.py

--- pages/1_🚀Predict.py
+++ pages/1_🚀Predict.py
@@ -51,25 +51,25 @@ def show_decide_page():
     st.sidebar.header("🔍 Input Features")
 
     # Sidebar sliders for user input
-    Distance_from_Earth = st.sidebar.slider("Distance from Earth (M km)", 1.0, 1000.0, 100.0)
-    Iron = st.sidebar.slider("Iron (%)", 0.0, 100.0, 50.0)
-    Nickel = st.sidebar.slider("Nickel (%)", 0.0, 100.0, 50.0)
-    Water_Ice = st.sidebar.slider("Water Ice (%)", 0.0, 100.0, 50.0)
-    Other_Minerals = st.sidebar.slider("Other Minerals (%)", 0.0, 100.0, 50.0)
-    Estimated_Value = st.sidebar.slider("Estimated Value (B USD)", 0.0, 500.0, 100.0)
-    Sustainability_Index = st.sidebar.slider("Sustainability Index", 0.0, 100.0, 50.0)
-    Efficiency_Index = st.sidebar.slider("Efficiency Index", 0.0, 100.0, 50.0)
+    distance_from_earth = st.sidebar.slider("Distance from Earth (M km)", 1.0, 1000.0, 100.0)
+    iron = st.sidebar.slider("Iron (%)", 0.0, 100.0, 50.0)
+    nickel = st.sidebar.slider("Nickel (%)", 0.0, 100.0, 50.0)
+    water_ice = st.sidebar.slider("Water Ice (%)", 0.0, 100.0, 50.0)
+    other_minerals = st.sidebar.slider("Other Minerals (%)", 0.0, 100.0, 50.0)
+    estimated_value = st.sidebar.slider("Estimated Value (B USD)", 0.0, 500.0, 100.0)
+    sustainability_index = st.sidebar.slider("Sustainability Index", 0.0, 100.0, 50.0)
+    efficiency_index = st.sidebar.slider("Efficiency Index", 0.0, 100.0, 50.0)
 
     # Data preparation
     data = {
-        'Distance from Earth (M km)': Distance_from_Earth,
-        'Iron (%)': Iron,
-        'Nickel (%)': Nickel,
-        'Water Ice (%)': Water_Ice,
-        'Other Minerals (%)': Other_Minerals,
-        'Estimated Value (B USD)': Estimated_Value,
-        'Sustainability Index': Sustainability_Index,
-        'Efficiency Index': Efficiency_Index
+        'distance_from_earth': distance_from_earth,
+        'iron': iron,
+        'nickel': nickel,
+        'water_ice': water_ice,
+        'other_minerals': other_minerals,
+        'Estimated Value (B USD)': estimated_value,
+        'sustainability_index': sustainability_index,
+        'efficiency_index': efficiency_index
     }
 
     # Prediction button
@@ -83,7 +83,6 @@ def show_decide_page():
         
         if prediction[0] == 1:
             st.success("✅ **This is a Potential Mining Site!**")
-            
         else:
             st.error("❌ **This is Not a Potential Mining Site.**")
 
